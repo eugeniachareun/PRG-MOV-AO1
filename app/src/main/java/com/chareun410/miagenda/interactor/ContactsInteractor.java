ab89ac533314851b365153420bd232584de9a181
eugeniachareun <eugenia.chareun@gmail.com>
2025-04-20 12:56:22 -0300

    Create ContactsInteractor

---
+++ app/src/main/java/com/chareun410/miagenda/interactor/ContactsInteractor.java
@@ -0,0 +1,16 @@
+package com.chareun410.miagenda.interactor;
+
+import android.app.Activity;
+
+public class ContactsInteractor {
+    private Activity actividad;
+
+    public ContactsInteractor(Activity actividad) {
+        this.actividad = actividad;
+    }
+
+    public void searchContact(String searchText) {
+
+    }
+
+}
